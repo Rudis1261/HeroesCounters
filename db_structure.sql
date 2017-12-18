d36d835a7d3697d3f572fb79aed8d9ecdb295619
Rudi Strydom <drpain@users.noreply.github.com>
2017-12-18 17:47:28 +0200

    Update db_structure.sql

--- db_structure.sql
+++ db_structure.sql
@@ -1,4 +1,3 @@
-;-- http://sqlfiddle.com/#!9/99d052/15
 CREATE TABLE IF NOT EXISTS `trait` (
   `id` int(11) unsigned NOT NULL,
   `name` varchar(200) NOT NULL,
@@ -11,24 +10,26 @@ INSERT INTO `trait` (`id`, `name`, `label`, `weight`) VALUES
   (2, 'auto_attack', 'Auto Attack', 50),
   (3, 'spell_damage', 'Spell Damage', 50),
   (4, 'tanky', "High health", 50),
-  (4, 'percentile_damage', '% based damage', 50),
-  (5, 'blind', 'Blinds', 50),
-  (6, 'stasis', '(Stasis, Trapped, Eaten, VP, Temporal Loop, Root)', 50),
-  (7, 'protected', 'Protected', 50),
-  (8, 'cleanse', 'Cleanse', 50),
-  (9, 'iceblock', 'Ice block', 50),
-  (9, 'siege', 'Siege damage', 50),
-  (10, 'invulnerable', 'Invulnerable', 50),
-  (11, 'resets', 'Kill resets', 50),
-  (12, 'unstoppable', 'Unstoppable', 50),
-  (13, 'insivible', 'Invisible', 50),
-  (14, 'reveal', 'Reveals', 50),
-  (15, 'shield', 'Shields', 50),
-  (16, 'blink', 'Blink', 50),
-  (17, 'slow', 'Slows', 50),
-  (18, 'stun', 'Stuns', 50),
-  (19, 'jungler', 'Jungler', 50),
-  (20, 'globe_quest', 'Globe Quest', 50);
+  (5, 'percentile_damage', '% based damage', 50),
+  (6, 'blind', 'Blinds', 50),
+  (7, 'stasis', '(Stasis, Trapped, Eaten, VP, Temporal Loop, Root)', 50),
+  (8, 'protected', 'Protected', 50),
+  (9, 'cleanse', 'Cleanse', 50),
+  (10, 'iceblock', 'Ice block', 50),
+  (11, 'siege', 'Siege damage', 50),
+  (12, 'invulnerable', 'Invulnerable', 50),
+  (13, 'resets', 'Kill resets', 50),
+  (14, 'unstoppable', 'Unstoppable', 50),
+  (15, 'insivible', 'Invisible', 50),
+  (16, 'reveal', 'Reveals', 50),
+  (17, 'shield', 'Shields', 50),
+  (18, 'blink', 'Blink', 50),
+  (19, 'slow', 'Slows', 50),
+  (20, 'stun', 'Stuns', 50),
+  (21, 'jungler', 'Jungler', 50),
+  (22, 'globe_quest', 'Globe Quest', 50),
+  (23, 'ranged', 'Ranged', 50),
+  (24, 'melee', 'Melee', 50);
   
   
   
@@ -67,14 +68,13 @@ CREATE TABLE IF NOT EXISTS `hero_trait` (
 ) DEFAULT CHARSET=utf8;
 -- hero trait flat table
 INSERT INTO `hero_trait` (`id`, `hero_id`, `trait_id`) VALUES
-  -- Ana
   (1, 1, 5),
   (2, 1, 6),
-  -- Diablo
   (3, 3, 1),
   (4, 3, 4),
   (5, 3, 18),
-  (5, 3, 20);
+  (6, 3, 20),
+  (7, 3, 22);
   
   
 CREATE TABLE IF NOT EXISTS `hero_mobility` (
@@ -85,10 +85,7 @@ CREATE TABLE IF NOT EXISTS `hero_mobility` (
 ) DEFAULT CHARSET=utf8;
 -- hero mobility flat table
 INSERT INTO `hero_mobility` (`id`, `hero_id`, `mobility_id`) VALUES
-  -- Ana
   (1, 1, 1),
-  -- Thrall
   (2, 2, 2),
-  -- Diablo
   (3, 3, 3);
   
