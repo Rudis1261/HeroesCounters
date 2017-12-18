315b3e99fa8ac788c8755717dd56d986473c607c
Rudi Strydom <drpain@users.noreply.github.com>
2017-12-18 17:47:09 +0200

    Update db_queries.sql

--- db_queries.sql
+++ db_queries.sql
@@ -1,3 +1,4 @@
+-- http://sqlfiddle.com/#!9/8a1aa7/5/0
 select * from `trait`;
 select * from `mobility`;
 select * from `hero`;
@@ -6,6 +7,7 @@ select * from `hero_trait`;
 SELECT 
   h.*, 
   group_concat(ht.trait_id separator ',') as `traits`,
+  sum(t.weight),
   (SELECT name FROM mobility WHERE id=m.id) as mobility
 FROM 
   `hero` h 
@@ -13,8 +15,13 @@ LEFT JOIN
   `hero_trait` ht 
 ON 
   h.id=ht.hero_id
-LEFT JOIN
+INNER JOIN
+  `trait` t
+ON
+  ht.trait_id=t.id
+INNER JOIN
   `hero_mobility` m
 ON
   h.id=m.hero_id
-GROUP by h.id;
+GROUP by h.id
+ORDER by h.name ASC
