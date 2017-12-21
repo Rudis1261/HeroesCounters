19c41a23ecb3bb48bcf445af388553ce015d0a05
Rudi Strydom <iam@thatguy.co.za>
2017-12-22 00:13:58 +0200

    UPdates

--- app/helpers/draft.rb
+++ app/helpers/draft.rb
@@ -12,4 +12,19 @@ module DraftHelper
 
     session[:draft][:picks] << pick
   end
+
+  def self.pick_order session
+    return if !session[:draft] || !session[:draft][:first_pick]
+
+    # red ban
+    # blue ban
+    # red 1 pick
+    # blue 2 picks
+    # red 2 picks
+    # blue ban
+    # red ban
+    # blue 2 picks
+    # red 2 picks
+    # blue 1 pick
+  end
 end
